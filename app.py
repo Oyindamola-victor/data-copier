bba6764437383a1b860d232b461c77e09905951c
Oyindamola-victor <damolavictor0@gmail.com>
2022-07-06 22:16:45 +0100

    Updated to process multiple tables

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 # This integrates the read and write logic to load a json file into a Database
 
 import os
+import sys
 from read import get_json_reader
 from write import load_db_table
 
 
-def main():
-    BASE_DIR = os.environ.get('BASE_DIR')
-    table_name = os.environ.get('TABLE_NAME')
+def process_table(BASE_DIR, conn, table_name):
     json_reader = get_json_reader(BASE_DIR,table_name)
-    configs = dict(os.environ.items())
-    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
-    
     for df in json_reader:
         load_db_table(df, conn, table_name, df.columns[0])
+
+
+def main():
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(',')
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+        
+    for table_name in table_names:
+        process_table(BASE_DIR, conn, table_name)
 
     
 if __name__ == "__main__":
